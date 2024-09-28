d8228e5cd0471f0d51065bfbfa05b96f2e4dcd2b
Burdyko Nazar <burdykonazar@gmail.com>
2024-09-28 12:22:13 +0300

    Задание 7

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Test
+{
+    int test1;
+    string test2;
+
+    public Test()
+    {
+        test1 = 0;
+        test2 = "Undefined";
+    }
+
+    public Test(int test1, string test2)
+    {
+        this.test1 = test1;
+        this.test2 = test2;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"параметр 1 = {this.test1}, параметр 2 = {this.test2}");
+    }
+}
